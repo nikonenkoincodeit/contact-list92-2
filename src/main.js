b91e64f96f376381d5e4d7a2832bc0cf974bcfe6
Vaku1a <110764648+Vaku1a@users.noreply.github.com>
2023-10-29 10:40:56 +0200

    form-data

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+import { formEl } from "./refs"
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+formEl.addEventListener('submit', onFormInput)
+
+function onFormInput(e) {
+    e.preventDefault();
+    
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+    e.target.reset()
+    console.log(data);
+
+}
